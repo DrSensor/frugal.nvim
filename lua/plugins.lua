92e16ba87912329b50a14f836dd03e99985c33e0
Fahmi Akbar Wildana <sensorfied@gmail.com>
2022-01-26 13:13:45 +0700

    Add gitsigns.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,4 +37,9 @@ return require('packer').startup(function(use)
     end
   }
 
+  use {
+    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup() end
+  }
+
 end)
