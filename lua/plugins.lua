94ea9d31d24a164ae81b0216110b541a3c8046a9
Fahmi Akbar Wildana <sensorfied@gmail.com>
2023-10-07 15:54:56 +0700

    yosh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,7 +4,52 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Packer can manage itself
+  -- use 'wbthomason/packer.nvim'
 
+  -- Simple plugins can be specified as strings
+  -- use '9mm/vim-closer'
+
+  -- Lazy loading:
+  -- Load on specific commands
+  -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+
+  -- Load on an autocommand event
+  -- use {'andymass/vim-matchup', event = 'VimEnter'}
+
+  -- Load on a combination of conditions: specific filetypes or commands
+  -- Also run code after load (see the "config" key)
+  -- use { -- not needed!
+  --   'w0rp/ale',
+  --   ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+  --   cmd = 'ALEEnable',
+  --   config = 'vim.cmd[[ALEEnable]]'
+  -- }
+
+  -- Plugins can have dependencies on other plugins
+  -- use { -- TODO: replace with neovim/nvim-lspconfig
+  --   'haorenW1025/completion-nvim',
+  --   opt = true,
+  --   requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+  -- }
+
+  -- Plugins can also depend on rocks from luarocks.org:
+  -- use {
+  --   'my/supercoolplugin',
+  --   rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
+  -- }
+
+  -- You can specify rocks in isolation
+  -- use_rocks 'penlight'
+  -- use_rocks {'lua-resty-http', 'lpeg'}
+
+  -- Local plugins can be included
+  -- use '~/projects/personal/hover.nvim'
+
+  -- Plugins can have post-install/update hooks
+  -- use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {
     'nvim-treesitter/playground', run = ':TSInstall query', after = 'nvim-treesitter',
@@ -75,6 +120,10 @@ return require('packer').startup(function(use)
     -- end
   }
 
+  -- Post-install/update hook with call of vimscript function with argument
+  -- use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+  -- Use specific branch, dependency and run lua file after load
   use {
     'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     -- config = function() require'statusline' end
@@ -259,6 +308,7 @@ return require('packer').startup(function(use)
     -- end
   }
 
+  -- Use dependency and run lua function after load
   use {
     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
     -- config = function() require('gitsigns').setup() end
@@ -338,4 +388,9 @@ return require('packer').startup(function(use)
     -- end
   }
 
+  -- You can specify multiple plugins in a single call
+  -- use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
+
+  -- You can alias plugin names
+  -- use {'dracula/vim', as = 'dracula'}
 end)
